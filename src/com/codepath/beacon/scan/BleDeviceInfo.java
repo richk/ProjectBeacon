cbe8f57dfdbe4451bbff37d9679dfbf71dc61d28
Richa Khandelwal <kricha@lab126.com>
2014-07-09 00:36:22 -0700

    Beacon list divided up into beacons saved by the user and new beacons. Dialog fragment to add new beacons and update existing beacons.

--- src/com/codepath/beacon/scan/BleDeviceInfo.java
+++ src/com/codepath/beacon/scan/BleDeviceInfo.java
@@ -18,9 +18,9 @@ public class BleDeviceInfo implements Parcelable {
   private int majorId;
 
   private int minorId;
-
   public BleDeviceInfo(String name, String macAddress, String uuid,
       int majorId, int minorId, int rssi) {
+  
     this.name = name;
     this.macAddress = macAddress;
     this.uuid = uuid;
