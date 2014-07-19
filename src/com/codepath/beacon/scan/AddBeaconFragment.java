de24f44d3502f5c46e24b5cfd9457a4d061b547d
Madhur Khandelwal <madhurk@gmail.com>
2014-07-19 06:24:17 -0700

    improve the layout of Select Beacon dialog fragment

--- src/com/codepath/beacon/scan/AddBeaconFragment.java
+++ src/com/codepath/beacon/scan/AddBeaconFragment.java
@@ -1,15 +1,19 @@
 package com.codepath.beacon.scan;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.beacon.R;
 
@@ -20,7 +24,8 @@ public class AddBeaconFragment extends DialogFragment {
     private TextView tvMajorId;
     private TextView tvMinorId;
     private EditText etDeviceName;
-    private Button btnAddBeacon;
+    private ImageView btnAddBeacon;
+    private ImageView btnDismissBeacon;
     
     public interface OnAddBeaconListener {
     	public void onBeaconAdded(BleDeviceInfo deviceInfo);
@@ -38,6 +43,15 @@ public class AddBeaconFragment extends DialogFragment {
     }
     
     @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+      Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+      // request a window without the title
+      dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+      return dialog;
+    }
+    
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
     	Bundle args = getArguments();
@@ -47,24 +61,39 @@ public class AddBeaconFragment extends DialogFragment {
     	tvMacAddress = (TextView) view.findViewById(R.id.tv_add_beacon_mac);
     	tvMajorId = (TextView) view.findViewById(R.id.tv_add_beacon_major_id);
     	tvMinorId = (TextView) view.findViewById(R.id.tv_add_beacon_minor_id);
-    	btnAddBeacon = (Button) view.findViewById(R.id.btn_add_beacon);
+    	btnAddBeacon = (ImageView) view.findViewById(R.id.btn_confirm);
     	btnAddBeacon.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				String name = etDeviceName.getText().toString();
+				if(name == null || name.trim().length() == 0){
+				  Toast.makeText(getActivity(), "Please give a name to the beacon", Toast.LENGTH_SHORT).show();
+				  dismiss();				  
+				  return;
+				}
+				
 				if (!name.equals(deviceInfo.getName())) {
 					deviceInfo.setEditState(true);
 				}
-				if (name!=null && !name.isEmpty()) {
-			        deviceInfo.setName(name);	
-				}
+				
+				deviceInfo.setName(name);
 				OnAddBeaconListener listener = (OnAddBeaconListener) getActivity();
 				listener.onBeaconAdded(deviceInfo);
 				dismiss();
 			}
 		});
-    	getDialog().setTitle(args.getString("title"));
+    	
+    	btnDismissBeacon = (ImageView)view.findViewById(R.id.btn_cancel);
+    	btnDismissBeacon.setOnClickListener(new OnClickListener() {
+          
+          @Override
+          public void onClick(View arg0) {
+              dismiss();
+          }
+      });
+        
+    	//getDialog().setTitle(args.getString("title"));
         if (deviceInfo.getName() != null) {
         	etDeviceName.setText(deviceInfo.getName());
         }
