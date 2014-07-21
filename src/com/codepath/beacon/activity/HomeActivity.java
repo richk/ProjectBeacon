e77890bbd33dea7cddfdd7a453209f2a93221828
rcao137 <rong_cao@yahoo.com>
2014-07-20 17:44:09 -0700

    added speech bubble to recipe detail and added home page background

--- src/com/codepath/beacon/activity/HomeActivity.java
+++ src/com/codepath/beacon/activity/HomeActivity.java
@@ -2,6 +2,7 @@ package com.codepath.beacon.activity;
 
 import com.codepath.beacon.R;
 import com.codepath.beacon.R.layout;
+import com.parse.ParseUser;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
 
@@ -16,6 +18,18 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+		  handleSuccessfulLogin();
+		}
+	}
+
+	protected void handleSuccessfulLogin() {
+		Toast.makeText(getApplicationContext(), "Hooray..the user can login", Toast.LENGTH_SHORT).show();
+		Intent scanIntent = new Intent(this, MyRecipeActivity.class);
+		startActivity(scanIntent);
+		
 	}
 	
 	public void onSignin(View view) {
