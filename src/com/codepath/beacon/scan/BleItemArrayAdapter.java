421be7b784fd31c2022fa37425c28bf2ff45467e
Richa Khandelwal <kricha@lab126.com>
2014-07-26 16:46:45 -0700

    Merged the ble_list_item for saved and new devices.

--- src/com/codepath/beacon/scan/BleItemArrayAdapter.java
+++ src/com/codepath/beacon/scan/BleItemArrayAdapter.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.opengl.Visibility;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +19,7 @@ public class BleItemArrayAdapter extends ArrayAdapter<BleDeviceInfo> {
 	private static final String LOG_TAG = BleItemArrayAdapter.class.getSimpleName();
 	private static final int DEFAULT_RSSI_VALUE = -150;
 	private final Context mContext;
-
+	
   public BleItemArrayAdapter(Context context, List<BleDeviceInfo> items) {
     super(context, 0, items);
     mContext = context;
@@ -30,11 +31,11 @@ public class BleItemArrayAdapter extends ArrayAdapter<BleDeviceInfo> {
 
     LayoutInflater inflator = LayoutInflater.from(getContext());
     View bleView = inflator.inflate(R.layout.ble_list_item, parent, false);
-    TextView tvRssi = (TextView)bleView.findViewById(R.id.tvRssi);
-    TextView tvUUID = (TextView)bleView.findViewById(R.id.tvUUID);
-    TextView tvName = (TextView)bleView.findViewById(R.id.tvName);
-    TextView tvMajorId = (TextView)bleView.findViewById(R.id.tvMajorId);
-    TextView tvMinorId = (TextView)bleView.findViewById(R.id.tvMinorId);
+    TextView tvRssi = (TextView) bleView.findViewById(R.id.tvRssi);
+    TextView tvName = (TextView) bleView.findViewById(R.id.tvName);
+    TextView tvUUID = (TextView) bleView.findViewById(R.id.tvUUID);
+    TextView tvMajorId = (TextView) bleView.findViewById(R.id.tvMajorId);
+    TextView tvMinorId = (TextView) bleView.findViewById(R.id.tvMinorId);
     
     int rssi = item.getRssi();
     if (item.getRssi() == 0) {
@@ -64,7 +65,9 @@ public class BleItemArrayAdapter extends ArrayAdapter<BleDeviceInfo> {
     tvRssi.setBackground(beaconDrawable);
     if (item.getName() != null) {
         tvName.setText(item.getName());
-    } 
+    } else {
+    	tvName.setVisibility(View.GONE);
+    }
     tvUUID.setText(item.getUUID());
     tvMajorId.setText(String.valueOf(item.getMajorId()));
     tvMinorId.setText(String.valueOf(item.getMinorId()));
