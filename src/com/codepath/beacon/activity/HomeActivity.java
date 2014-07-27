e300ac9cca25fa078d56ad739a69af83d27e7df5
Madhur Khandelwal <madhurk@gmail.com>
2014-07-26 19:59:03 -0700

    add crittercism for crash and exception reporting

--- src/com/codepath/beacon/activity/HomeActivity.java
+++ src/com/codepath/beacon/activity/HomeActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 
 import com.codepath.beacon.R;
+import com.crittercism.app.Crittercism;
 import com.parse.ParseUser;
+
 
 public class HomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Crittercism.initialize(getApplicationContext(), "53d465b0d478bc25cc000001");
 		setContentView(R.layout.activity_home);
 		
 		ParseUser currentUser = ParseUser.getCurrentUser();
@@ -26,8 +28,7 @@ public class HomeActivity extends Activity {
 	protected void handleSuccessfulLogin() {
 		//Toast.makeText(getApplicationContext(), "Hooray..the user can login", Toast.LENGTH_SHORT).show();
 		Intent scanIntent = new Intent(this, MyRecipeActivity.class);
-		startActivity(scanIntent);
-		
+		startActivity(scanIntent);	    
 	}
 	
 	public void onSignin(View view) {
